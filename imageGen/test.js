d38db6d21b4d26ca94876d583da9237ab4f00f97
Christopher Chin <aisman64@gmail.com>
2015-05-06 20:29:10 +0800

    add image generator

--- imageGen/test.js
+++ imageGen/test.js
@@ -4,36 +4,31 @@ var tmp = require('tmp');
 var cpro = require('child_process')
 var sys = require('sys');
 
-function createTmpFile()
-	{
-	console.log('Copy Done');
-	return tmpfile;
-	}
 
 function mntImg()
 	{
-	var offset = 195346*512;
+	var offset = 100017152;
 	var tmpfile = tmp.tmpNameSync({ template: '/tmp/debianImg-XXXXXXXX' });
 	var tmpdir = tmp.dirSync({ mode: '0750', template: '/tmp/debian-XXXXXXXX' });
 	fs.copySync('defaultImg/debian.img', tmpfile);
 	cpro.exec('losetup -o ' + offset + ' /dev/loop0 '+tmpfile);
 	console.log('Device Mounted');
 	cpro.exec('mount /dev/loop0 '+tmpdir.name);
-
+	var Img = {
+		tmpfile: tmpfile,
+		tmpdir: tmpdir.name
+		};
+	return Img;
 	}
 
 function umntImg(tmpdir, tmpfile)
 	{
 	cpro.exec('umount '+tmpdir.name);
 	cpro.exec('losetup -d /dev/loop0');
-	cpro.exec('gzip 
+	cpro.exec('gzip '+tmpdir.name); 
 	console.log('Device Unmounted');
 	}
 
-function removeTmp(tmpfile)
-	{
-	fs.deleteSync(tmpfile);
-	console.log('Delete Done');
-	}
 
 mntImg();
+//unmntImage();
