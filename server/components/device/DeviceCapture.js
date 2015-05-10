525267391f5f5a9b78947e5b52eca341d524aab0
Christopher Chin <aisman64@gmail.com>
2015-05-10 23:30:35 +0800

    V0.1 DeviceCapture

--- server/components/device/DeviceCapture.js
+++ server/components/device/DeviceCapture.js
@@ -25,29 +25,30 @@ module.exports = NoGapDef.component({
     /**
      * Everything defined in `Host` lives only on the host side (Node).
      */
-    Host: NoGapDef.defHost(function(SharedTools, Shared, SharedContext) { return {
-        /**
-         * 
-         */
-        initHost: function() {
-        },
+    Host: NoGapDef.defHost(function(SharedTools, Shared, SharedContext) { 
+        return {
+            __ctor: function() {
+            },
+            initHost: function() {
+            },
 
-        Private: {
-            onClientBootstrap: function() {
-            }
-        },
-        
-        /**
-         * Host commands can be directly called by the client
-         */
-        Public: {
-            storePacket: function(packet) {
-                if (!this.Instance.User.isDevice()) return Promise.reject('error.invalid.permissions');
+            Private: {
+                onClientBootstrap: function() {
+                }
+            },
+            
+            /**
+             * Host commands can be directly called by the client
+             */
+            Public: {
+                storePacket: function(packet) {
+                    if (!this.Instance.User.isDevice()) return Promise.reject('error.invalid.permissions');
 
-                return this.Instance.WifiPacket.storePacket(packet);
-            }
-        },
-    }}),
+                    return this.Instance.WifiPacket.storePacket(packet);
+                }
+            },
+        }
+    }),
     
     
     /**
@@ -55,25 +56,72 @@ module.exports = NoGapDef.component({
      */
     Client: NoGapDef.defClient(function(Tools, Instance, Context) {
         var ThisComponent;
-
+        var pcap;
+        var sys;
+        var exec;
+        var Queue;
         return {
             __ctor: function() {
                 ThisComponent = this;
+                pcap = require('pcap');
+                sys = require('sys');
+                exec = require('child_process').exec;
+                Queue = require('file-queue').Queue;
             },
 
             /**
              * This method is called by DeviceMain, once we are logged into the server!
              */
-            startCapturing: function() {
-                // TODO: Start capturing packets
+            puts: function(error, stdout, stderr) {
+                sys.puts(stdout); 
+            }, 
 
-                console.log("Let's capture something!");
-
-                // this.storePacket(packet);
+            storePackets: function(packets) {
+                // send packet to server
+                return this.host.storePackets(packets)
+                .then(function() {
+                    // DB successfully stored packet
+                })
+                .catch(function(err) {
+                    queue.push(packet, function(err) { 
+                        if(err) throw err; 
+                        queue.length(function(err,len) { console.log(len); });
+                    });
+                });
+            },
+ 
+            structToMac: function(struct) {
+                var result = "";
+                for(var i = 0; i<6; i++)
+                        {
+                        l = struct[i].toString(16);
+                        result += l.length === 1 ? '0' + l: l;
+		        }
+        	return result;
             },
 
-            storePacket: function(packet) {
-                return this.host.storePacket(packet);
+            preCapture: function() {
+	        exec("ntpdate -s ntp.nict.jp clock.tl.fukuoka-u.ac.jp clock.nc.fukuoka-u.ac.jp", ThisComponent.puts);
+            	exec("ntp-wait -v", ThisComponent.puts);
+                exec("iw phy phy0 interface add mon0 type monitor", ThisComponent.puts);
+                exec("ifconfig mon0 up", ThisComponent.puts);
+            },
+            processPacket: function(packet) {
+                var result = {};
+                result.mac = ThisComponent.structToMac(packet.payload.ieee802_11Frame.shost.addr);
+                result.mac = structToMac(packet.payload.ieee802_11Frame.shost.addr);
+                result.signalStrength = packet.payload.signalStrength;
+                result.time = packet.pcap_header.tv_sec+(packet.pcap_header.tv_usec/1000000);
+                result.seqnum = packet.payload.ieee802_11Frame.fragSeq >> 4;
+                result.ssid = packet.payload.ieee802_11Frame.probe.tags[0].ssid;
+                ThisComponent.storePacket(result): 
+            },
+            startCapturing: function() {
+                pcap_session = pcap.createSession("mon0", "wlan type mgt subtype probe-req");
+                pcap_session.on('packet', function(raw_packet) { 
+                        var packet = pcap.decode.packet(raw_packet);
+                        ThisComponent.processPacket(packet);
+                        });
             },
 
             onCurrentUserChanged: function(privsChanged) {
