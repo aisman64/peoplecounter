3192ad0ef3ef63d2c342d8ebc8a57cdca23cbf53
Domiii <dominik.seifert@gmail.com>
2015-05-17 23:20:45 +0800

    - added MACInfoElement for visualizing MACAddress-specific data and their SSID graph (not quite working yet); - added default hostName to DeviceConfig, if device did not have one set yet

--- server/components/models/queries/MostConnectedMACs.sql
+++ server/components/models/queries/MostConnectedMACs.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*) count, r.macId as macId, macAddress
 FROM MAC_SSID_Relation r
-INNER JOIN MacAddress a
+INNER JOIN MACAddress a
 ON (r.macId = a.macId)
 GROUP BY macId
 ORDER BY count DESC
--- server/components/models/queries/MostOftenSeenMACs.sql
+++ server/components/models/queries/MostOftenSeenMACs.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*) count, p.macId macId, macAddress
 FROM WifiSSIDPacket p
-INNER JOIN MacAddress a
+INNER JOIN MACAddress a
 ON (p.macId = a.macId)
 GROUP BY macId
 ORDER BY count DESC
