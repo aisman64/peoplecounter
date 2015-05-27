923e97f378eb76927facf1951a3872d00f503a4e
nkfly <nkflyfly@gmail.com>
2015-05-27 18:17:04 +0800

    move to live

--- server/components/ui/live/LivePage.js
+++ server/components/ui/live/LivePage.js
@@ -139,7 +139,83 @@ module.exports = NoGapDef.component({
                         console.error($scope.BottomPanelCurrentHeight);
                     };
 
+                    $scope.toggleHistoryGraph = function(isOpen) {
+                        isOpen = isOpen === undefined ? !$scope.HistoryGraphOpen : isOpen;
+                        $scope.HistoryGraphOpen = isOpen;
+                        // console.error($scope.BottomPanelCurrentHeight);
+                    };
+
+                    // $scope.HistoryGraphOpen = false;
+
                     $scope.toggleBottomPanel($scope.LayoutSettings.BottomPanelOpen);
+
+                    $scope.options = {
+                        chart: {
+                            type: 'lineChart',
+                            height: 600,
+                            margin : {
+                                top: 20,
+                                right: 20,
+                                bottom: 100,
+                                left: 55
+                            },
+                            x: function(d){ return d.x; },
+                            y: function(d){ return d.y; },
+                            useInteractiveGuideline: true,
+                            dispatch: {
+                                stateChange: function(e){ console.log("stateChange"); },
+                                changeState: function(e){ console.log("changeState"); },
+                                tooltipShow: function(e){ console.log("tooltipShow"); },
+                                tooltipHide: function(e){ console.log("tooltipHide"); }
+                            },
+                            xAxis: {
+                                axisLabel: 'Time'
+                            },
+                            yAxis: {
+                                axisLabel: 'People(Mac Address) Count',
+                                tickFormat: function(d){
+                                    return d3.format('d')(d);
+                                },
+                                axisLabelDistance: 30
+                            },
+                            callback: function(chart){
+                                console.log("!!! lineChart callback !!!");
+                            }
+                        },
+                        title: {
+                            enable: true,
+                            text: 'People Counter by Mac Address'
+                        },
+                        subtitle: {
+                            enable: false,
+                            text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+                            css: {
+                                'text-align': 'center',
+                                'margin': '10px 13px 0px 7px'
+                            }
+                        },
+                        caption: {
+                            enable: false,
+                            html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
+                            css: {
+                                'text-align': 'justify',
+                                'margin': '10px 13px 0px 7px'
+                            }
+                        }
+                    };
+
+                    
+
+                    /*Random Data Generator */
+                    
+
+
+                    $scope.data = [];
+
+                    
+
+
+
                 });
 
                 // register page
@@ -195,6 +271,7 @@ module.exports = NoGapDef.component({
                     ThisComponent.busy = false;
                 })
                 .then(function(deviceCounts) {
+                    console.log(deviceCounts);
                     ThisComponent.deviceCounts = deviceCounts;
                     ThisComponent.page.invalidateView();
                 })
